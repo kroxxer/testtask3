2e8360ed901ceb8458c238231fbfd5e08276e836
Aboba <valikcrazy@gmail.com>
2024-07-24 10:26:34 +0300

    a

--- src/index.php
+++ src/index.php
@@ -1,8 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="./main.js"></script>
+        <link href="./main.css" rel="stylesheet" />
+        <meta charset=utf-8>
+        <title>Журнал Расходов</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-cone-striped' viewBox='0 0 16 16'>
+        <path d='m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9s-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12m-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4s1.2-.036 1.725-.098m4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257z'/>
+        </svg>">
+    </head>
 <?php
 
 $request = $_SERVER['REQUEST_URI'];
 $connectionString = 'user=admin password=SXpr4qEcUwN3vvm6N7goZbUTRZeqbE7a host=dpg-coqcdi779t8c738e9b90-a.frankfurt-postgres.render.com dbname=users_6sfe port=5432 sslmode=require';
 $postgreConnection = pg_connect($connectionString);
+$methodName = 'POST';
 
 if ($postgreConnection)
     echo 'Connected';
@@ -10,23 +22,27 @@ if ($postgreConnection)
 switch ($request) {
     case '':
     case '/':
-        require __DIR__ . '/index.html';
+        require  __DIR__ . '/index.html';
         break;
 
     case '/sort':
-        require __DIR__ . '/users.php';
+        if ($_SERVER['REQUEST_METHOD'] === $methodName)
+            require __DIR__ . '/sort.php';
         break;
 
     case '/filter':
-        require __DIR__ . '/contact.php';
+        if ($_SERVER['REQUEST_METHOD'] === $methodName)
+            require __DIR__ . '/filter.php';
         break;
 
     case '/add':
-        require __DIR__ . '/add.php';
+        if ($_SERVER['REQUEST_METHOD'] === $methodName)
+            require __DIR__ . '/add.php';
         break;
     
     case '/show':
-        require __DIR__ . '/show.php';
+        if ($_SERVER['REQUEST_METHOD'] === $methodName)
+            require __DIR__ . '/show.php';
         break;
 }
 
